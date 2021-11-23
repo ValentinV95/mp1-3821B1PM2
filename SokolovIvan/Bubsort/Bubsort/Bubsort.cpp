7e071ec2edf92d6b6db0308d5c5e4ffc43346e5f
Your Name <you@example.com>
2021-11-23 15:44:06 +0300

    bubsort

---
+++ SokolovIvan/Bubsort/Bubsort/Bubsort.cpp
@@ -0,0 +1,32 @@
+﻿#include "stdio.h"
+#define N 10
+int main()
+{
+	float massiv[N];
+	int i, j, k, zamena, sravnen = 0;
+	for (i = 0; i < N; i++)
+	{
+		scanf_s("%f", &massiv[i]);
+	}
+	for (i = 0; i < N - 1; i++)
+	{
+		k = 0;
+		for (j = N - 2; j > i - 1; j--)
+		{
+			if (massiv[j] > massiv[j + 1])
+			{
+				zamena = massiv[j];
+				massiv[j] = massiv[j + 1];
+				massiv[j + 1] = zamena;
+				k = 1;
+			}
+			sravnen++;
+		}
+		if (k == 0)
+			break;
+	}
+	for (i = 0; i < N; i++)
+	{
+		printf("%f ", massiv[i]);
+	}
+}
