02b1ce0ecf864c104b74fd8b6eb07a3af53b0e4d
GuryevGrigory <g.guryev03@gmail.com>
2021-11-25 21:10:36 +0300

    Seletion_Sort.1.1

--- GuryevGrigory/Selection_sort/Selection_sort/Source.cpp
+++ GuryevGrigory/Selection_sort/Selection_sort/Source.cpp
@@ -1,30 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void Selection_Sort(int *Array)
+int Selection_Sort(float *Array,int size)
 {
-
-	for (int i = 0; i < 9; i++)
+	int ng = 0;
+	for (int i = 0; i < size-1; i++)
 	{
 		int min_index = i;
-		for (int j = i + 1; j < 10; j++)
+		for (int j = i + 1; j < size; j++)
 		{
+			ng=ng + 1;
 			if (*(Array+min_index) > *(Array+j))
 			{
 				min_index = j;
 			}
 		}
-		int change_var;
+		double change_var;
 		change_var = *(Array+min_index);
 		*(Array+min_index) = *(Array+i);
 		*(Array+i) = change_var;
 
 	}
+	return ng;
 }
 
 int main()
 {
-	int Array[10] = { 0,11,12,3,4,7,6,5,9,8 };
+	srand(time(NULL));
+	int size = 100;
+	int n = 0;
+	float Array[100];//= { 0,0,0,0,0,0,0,0,0,0 };
+	for (int i = 0; i < 100; i++)
+	{
+		int a = rand() % 10000;
+		float f =  a/ 100.0f;
+		Array[i] = f;
+	}
 	/*
 	for (int i = 0; i < 9; i++)
 	{
@@ -43,10 +56,18 @@ int main()
 		Array[min_index] = Array[i];
 		Array[i] = change_var;
 	}*/
-	Selection_Sort(Array);
-	for (int z = 0; z < 10; z++)
+	for (int z = 0; z < 100; z++)
 	{
-		printf("%i, ", Array[z]);
+		printf("%f, ", Array[z]);
 	}
+	printf("\n");
+	n = Selection_Sort(Array,size);
+	for (int z = 0; z < 100; z++)
+	{
+		printf("%f, ", Array[z]);
+	}
+	printf("\n");
+	printf("%i", n);
+	//printf("%f Number of iterations:", n);
 	return 0;
 }
