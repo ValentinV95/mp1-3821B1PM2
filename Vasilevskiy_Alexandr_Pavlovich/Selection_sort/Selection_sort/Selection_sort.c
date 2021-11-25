c3d77ced8ebdde409486490603e7c5f54866b268
GitDefeatedMe <vasilevsijsasa@gmail.com>
2021-11-25 18:09:16 +0300

    test

---
+++ Vasilevskiy_Alexandr_Pavlovich/Selection_sort/Selection_sort/Selection_sort.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int i, j, minID;
+	double a[10], tmp;
+	srand(777);
+	for (i = 0; i < 10; i++)
+	{
+		a[i] = rand()%1000000 + (rand() % 1000000) / 1000000.0;
+		printf("a[%d]%lf\n",i,a[i]);
+	}
+
+	printf("Sort:\n");
+
+	for (i = 0; i < 10 - 1; i++)
+	{
+		minID = i;
+		for (j = i + 1; j < 10; j++)
+		{
+			if (a[minID] > a[j])
+			{
+				minID = j;
+			}
+		}
+		tmp = a[i];
+		a[i] = a[minID];
+		a[minID] = tmp;
+	}
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("a[%d]%lf\n", i, a[i]);
+	}
+	
+}
---
+++ Vasilevskiy_Alexandr_Pavlovich/comb_sort/comb_sort/comb_sort.c
@@ -0,0 +1,46 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+
+void comb_sort(double array[], int size)
+{
+    double factor = 1.2473309;
+    int step = size - 1;
+
+    while (step >= 1)
+    {
+        for (int i = 0; i + step < size; i++)
+        {
+            if (array[i] > array[i + step])
+            {
+                double tmp = array[i];
+                array[i] = array[i + step];
+                array[i + step] = tmp;
+            }
+        }
+        step /= factor;
+    }
+}
+
+void print(double array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("a[%d] = %lf\n", i, array[i]);
+    }
+}
+
+int main()
+{
+    double a[15];
+    int i;
+    srand(1000);
+    for (i = 0; i < 15; i++)
+    {
+        a[i] = rand() % 10000000 + rand()%10000000 / 10000000.0;
+    }
+    print(a, 15);
+    printf("sorted : \n");
+    comb_sort(a, 15);
+    print(a, 15);
+}
