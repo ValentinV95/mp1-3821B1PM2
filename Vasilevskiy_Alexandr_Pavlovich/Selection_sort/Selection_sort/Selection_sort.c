cc8a899ed10a9af469d6820f8ca1f6ba26a77d45
GitDefeatedMe <vasilevsijsasa@gmail.com>
2021-12-05 23:57:49 +0300

    change selection sort

--- Vasilevskiy_Alexandr_Pavlovich/Selection_sort/Selection_sort/Selection_sort.c
+++ Vasilevskiy_Alexandr_Pavlovich/Selection_sort/Selection_sort/Selection_sort.c
@@ -1,37 +1,42 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void selectionSort(double* arr, int len)
 {
 	int i, j, minID;
-	double a[10], tmp;
-	srand(777);
-	for (i = 0; i < 10; i++)
-	{
-		a[i] = rand()%1000000 + (rand() % 1000000) / 1000000.0;
-		printf("a[%d]%lf\n",i,a[i]);
-	}
-
-	printf("Sort:\n");
-
-	for (i = 0; i < 10 - 1; i++)
+	double tmp;
+	for (i = 0; i < len - 1; i++)
 	{
 		minID = i;
-		for (j = i + 1; j < 10; j++)
+		for (j = i + 1; j < len; j++)
 		{
-			if (a[minID] > a[j])
+			if (arr[minID] > arr[j])
 			{
 				minID = j;
 			}
 		}
-		tmp = a[i];
-		a[i] = a[minID];
-		a[minID] = tmp;
+		tmp = arr[i];
+		arr[i] = arr[minID];
+		arr[minID] = tmp;
 	}
+}
 
-	for (i = 0; i < 10; i++)
+int main()
+{
+	int i, len = 10;
+	double* a;
+	srand(777);
+
+	a = (double*)malloc(len * sizeof(double));
+	for (i = 0; i < len; i++)
 	{
-		printf("a[%d]%lf\n", i, a[i]);
+		a[i] = rand() % 1000000 + (rand() % 1000000) / 1000000.0;
+		printf("a[%d] = %lf;\n", i, a[i]);
 	}
-	
+	selectionSort(a, len);
+	for (i = 0; i < len; i++)
+	{
+		printf("a[%d] = %lf;\n", i, a[i]);
+	}
+	return 0;
 }
