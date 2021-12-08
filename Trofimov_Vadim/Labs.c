2ee890cc95830166d8b2ed412a2c8e1dec7cf4e1
SirTruber <mega.boss.vadim@mail.ru>
2021-12-09 01:26:37 +0300

    2/4 + some comments

--- Trofimov_Vadim/Labs.c
+++ Trofimov_Vadim/Labs.c
@@ -1,43 +1,101 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <time.h>
+int perm = 0, comp = 0;
 
-double Randomize(double a[], int size) {
+double* Randomize(double a[], int size) {
 	int i;
 	srand(time(NULL));
 	for (i = 0; i < size; i++) {
-		a[i] = (double)rand() / (double)rand();
+		a[i] = (double)rand() / (double)rand(); // задаём случайное дробное число
 	}
-	return *a;
+	return a;
 }
 
 void Selection(double a[],int size) {
-	int i,j, min;
+	int i,j, min, perm = 0,comp = 0;
 	double tmp;
 	for (i = 0; i < size - 1; i++) {
 		min = i;
 		for (j = i + 1; j < size; j++) {
+			comp++;
 			if (a[j] < a[min]) {
 				min = j;					//Находим минимальный элемент в массиве
 			}
 		}
+		comp++;
 		if (min != i) {						// Ставим минимальный элемент в начало
 			tmp = a[i];
 			a[i] = a[min];
 			a[min] = tmp;
+			perm++;
 		}
 	}
 	for (i = 0; i < size; i++) {
 		printf("%lf /", a[i]);
 	}
+	printf("Number of permition %d\n", perm);
+	printf("Number of comparisons %d\n", comp);
+	system("pause");
 }
 
 void Shell(double a[], int size) {
+	int s, i, j;
+	double tmp;
+	perm = 0; comp = 0;
+	for (s = size / 2; s > 0; s /= 2) {  //Выбираем шаг между элементами
+		for (i = s; i < size; i++) {
+			comp++;
+			for (j = i - s; j >= 0 && a[j] > a[j + s]; j -= s) { //Сравниваем элементы и меняем нужные
+				tmp = a[j];
+				a[j] = a[j + s];
+				a[j + s] = tmp;
+				perm++;
+			}
+		}
+	}
 
+	for (i = 0; i < size; i++) {
+		printf("%lf /", a[i]);
+	}
+	printf("Number of permition %d\n", perm);
+	printf("Number of comparisons %d\n", comp);
+	system("pause");
 }
 
-void Merge(double a[], int size) {
+void Merge(double a[], int left,int mid, int right) {
+	int size = right - left + 1, i = left, j = mid + 1, k = 0,f;
+	double *tmp = (double*)malloc(size * sizeof(double));
+	while (i <= mid && j <= right) {
+		if (a[i] < a[j]) {
+			tmp[k++] = a[i++];
+		}
+		else {
+			tmp[k++] = a[j++];
+		}
+	}
+		for (; i <= mid;) {
+			tmp[k++] = a[i++];
+		}
+		for (; j <= right;) {
+			tmp[k++] = a[j++];
+		}
+		for(f=0;f<=size;f++){
+	    a[f]= tmp[f];
+		printf("%lf", a[f]);
+	}
+	
+}
 
+void mergesort(double* a, int left, int right) {
+	int mid = (left + right) / 2,i;  //найди и перепиши по- человечески
+	if (left >= right) {
+		return;
+	}
+	mergesort(a, left, mid);
+	mergesort(a, mid + 1, right);
+
+	Merge(a, left, mid, right);
 }
 
 void Radix(double a[], int size) {
@@ -45,33 +103,53 @@ void Radix(double a[], int size) {
 }
 
 void main() {
-	int i,size,variant, *a = NULL;
-	printf("enter the size of the array");
-	scanf(" %d", &size);
-	a = (double*)malloc(size * sizeof(double));
+	int size, variant,i;
+	double *a = NULL;
+	printf("enter the size of the array\n");
+	scanf_s(" %d", &size);
+	a = (double*) malloc(size * sizeof(double));
+	Randomize(a, size);
 	do
 	{
 		system("cls");
-		printf("Select the sort type/n");
-		printf("1.Selection/n");
-		printf("2.Shell");
-		printf("3.Merge");
-		printf("4.Radix");
-		printf("5.Randomize");
-		printf("0.Exit");
-		scanf(" %d", &variant);
+		printf("Select the sort type\n");
+		printf("1.Selection\n");
+		printf("2.Shell\n");
+		printf("3.Merge\n");
+		printf("4.Radix\n");
+		printf("5.Randomize array\n");
+		printf("6.Change size\n");
+		printf("0.Exit\n");
+		scanf_s(" %d", &variant);
 			switch (variant)
 			{
 			case 1:
-				Selection(&a, size);
+				Selection(a, size);
 				break;
 			case 2:
+				Shell(a, size);
 				break;
 			case 3:
+				mergesort(a,0,size-1);
+				printf("Number of permition %d\n", perm);
+				printf("Number of comparisons %d\n", comp);
+				for (i = 0; i < size; i++) {
+					printf("%lf /", a[i]);
+				}
+				system("pause");
 				break;
 			case 4:
+				Radix(a, size);
 				break;
 			case 5:
+				Randomize(a, size);
+				break;
+			case 6:
+				free(a);
+				printf("enter the size of the array\n");
+				scanf_s(" %d", &size);
+				a = (double*)malloc(size * sizeof(double));
+				Randomize(a, size);
 				break;
 			case 0:
 				break;
@@ -79,6 +157,6 @@ void main() {
 				printf("incorrent choice"); // проверка на ошибочные вводы
 				break;
 			}
-	} while (variant==0)
+	} while (variant != 0);
 	
 }
