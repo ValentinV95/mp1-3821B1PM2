027fa4741c60d01d63d957d74c9ed7ee667dfb27
LoveCheeks <nagornayaelenaal@gmail.com>
2021-12-18 13:45:44 +0300

    RadixSort

--- NagornayaElena/CombSort/CombSort.cpp
+++ NagornayaElena/CombSort/CombSort.cpp
@@ -4,10 +4,11 @@ int main()
 {
 	int i;
 	const int n = 30;
-	float A[n];
+	float A[n], B[n];
 	srand(777);
 	for (i = 0; i < n; i++) {
 		                    A[i] = rand() % 256;
+							B[i] = A[i];
 		                    printf("%.1f  ", A[i]);
 	                        }
 	double factor = 1.2473309;
@@ -24,5 +25,24 @@ int main()
 	                   }
 	printf("\n");
 	for (i = 0; i < n; i++) { printf("%.1f  ", A[i]); }
+
+	printf("\n");
+	for (i = 0; i < n; i++) { printf("%.1f  ", A[i]); }
+	for (i = 0; i < (n - 1); i++)
+		for (j = n - 2; j > (i - 1); j--)
+			if (B[j] > B[j + 1]) {
+				float tmp = B[j];
+				B[j] = B[j + 1];
+				B[j + 1] = tmp;
+			}
+	int k = 0;
+	printf("\n");
+	for (i = 0; i < n; i++) {
+		if (A[i] == B[i]) { k = k + 1; };
+		printf("%.1f  ", B[i]);
+	}
+	printf("\n");
+	if (k == n) { printf("Correct"); }
+	else { printf("No correct"); }
 	return 0;
 }
--- NagornayaElena/InsertionSort/InsertionSort.cpp
+++ NagornayaElena/InsertionSort/InsertionSort.cpp
@@ -4,10 +4,11 @@ int main()
 {
 	int i,j;
 	const int n = 10;
-	float A[n];
-	srand(777);
+	float A[n], B[n];
+	srand(800);
 	for (i = 0; i < n; i++) {
 		                    A[i] = rand()%256;
+							B[i] = A[i];
 							printf("%.1f  ", A[i]);
                              }
 	for (i = 1; i < n; i++)  
@@ -18,5 +19,22 @@ int main()
 		                                               }
 	printf("\n");
 	for (i = 0; i < n; i++) { printf("%.1f  ", A[i]); }
+	for (i=0; i<(n-1); i++) 
+		for (j=n-2; j>(i-1);j--)
+			if (B[j] > B[j + 1]) {
+				                 float tmp = B[j];
+								 B[j] = B[j + 1];
+								 B[j + 1] = tmp;
+								 }
+	int k = 0;
+	printf("\n");
+	for (i = 0; i < n; i++) { 
+		                     if (A[i] == B[i]) { k = k + 1; };
+	                       	 printf("%.1f  ", B[i]); 
+	                         }
+	printf("\n");
+	if (k == n) { printf("Correct"); }
+	else {printf("No correct"); }
+
 	return 0;
 }
