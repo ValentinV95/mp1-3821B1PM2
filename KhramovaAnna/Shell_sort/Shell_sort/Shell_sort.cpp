cf73dbf58274e5caa3b462e4f9e4ba52de5c44b6
annakhramovaa <anahramova0@gmail.com>
2021-12-19 14:34:20 +0300

    ShellSort

--- KhramovaAnna/Shell_sort/Shell_sort/Shell_sort.cpp
+++ KhramovaAnna/Shell_sort/Shell_sort/Shell_sort.cpp
@@ -1,40 +1,55 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
-int main() {
-	const int n = 20000;
-	double array[n];
-	int i = 0, j = 0,step = 0;
-	int comp = 0, swap = 0;
-	double tmp = 0;
-	//for (i = 0; i < n; i++) {
-	//	printf("a[%d] = ", i);
-	//	scanf_s("%lf", &array[i]);
-	//}
-	srand(777);
-	for (i = 0; i < n; i++) {
-		printf("a[%d] = ", i);
-		array[i] = rand()%200;
+int swap = 0, comp = 0;
+void check_array(double array[], int size)
+{
+	for (int i = 1; i < size; i++)
+	{
+		if (array[i - 1] > array[i])
+		{
+			printf("\nSort is completed incorrectly\n");
+			break;
+		}
+		else
+		{
+			printf("\nSort is correctly completed\n");
+			break;
+		}
 	}
+}
 
-	for (step = n / 2; step > 0; step = step / 2) {
-		comp = comp + 1;
-		for (i = step; i < n; i++) {
+void Shell_sort(double array[], int size) {
+	for (int step = size / 2; step > 0; step = step / 2) {
+		for (int i = step; i < size; i++) {
 			comp = comp + 1;
-			for (j = i-step; j >= 0 && array[j] > array[j+step]; j = j - step){
-				comp = comp + 2;
-				swap = swap + 3;
-				tmp = array[j];
+			for (int j = i - step; j >= 0 && array[j] > array[j + step]; j = j - step) {
+				double tmp = array[j];
 				array[j] = array[j + step];
 				array[j + step] = tmp;
-
+				swap = swap + 3;
+				comp = comp + 2;
 			}
 		}
 	}
+}
+int main() {
+	const int n = 100;
+	double a[n];
+	int i = 0, j = 0;
+	double tmp = 0;
+	for (i = 0; i < n; i++) {
+		printf("a[%d] = ", i);
+		a[i] = (double)(rand()) / RAND_MAX * (2000) - 1000;
+		printf("%lf\n", a[i]);
+	}
+	Shell_sort(a, n);
 	printf("\nsorted array:\n");
 	for (i = 0; i < n; i++) {
-		printf("a[%d] = %.1lf\n", i, array[i]);
+		printf("a[%d] = %lf\n", i, a[i]);
 	}
-	printf("the number of comparisons = %d", comp);
-	printf("the number of swaps = %d", swap);
+	printf("cheking the correctness of sort:\n");
+	check_array(a, n);
+	printf("the number of comparisons = %d\n", comp);
+	printf("the number of swap = %d", swap);
 	return 0;
 }
