297a6c8e8312d45ba844519b8fefa0487f5eb154
JuliaAnisimova <yulia.ainisimova@yandex.ru>
2021-12-19 16:13:29 +0300

    all sorts

---
+++ AnisimovaJulia/Buble sort my/Buble sort my/Buble sort my.c
@@ -0,0 +1,52 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#define SIZE 10
+
+int main()
+{
+	double arr[SIZE], num;
+    int p, i, n = 0, sravn = 0, perest = 0;
+
+	printf("Massiv: \n");
+	// Вводим значиния элементов массива
+	for (i = 0; i < SIZE; i++)
+	{
+		printf("arr[%d] = ", i);
+		scanf("%lf", &arr[i]);
+	}
+    printf("\n Sorted array: \n");
+    //Сортировка 
+    for (i = 0; i < SIZE; i++)
+    {
+        for (p = 0; p < SIZE - i - 1; p++)
+        {
+            sravn++;
+            if (arr[p] > arr[p + 1])
+            {
+                perest++;
+                num = arr[p];
+                arr[p] = arr[p + 1];
+                arr[p + 1] = num;
+                
+                
+            }
+            
+           
+        }
+    }
+
+    for (i = 0; i < SIZE; i++)
+    {
+        printf("%.5f \n", arr[i]);
+    }
+
+    printf("\n Number of sravneny = %d", sravn);
+    printf("\n Number of perestanovok = %d", perest);
+
+    return 0;
+}
+
+
+
---
+++ AnisimovaJulia/OuickSort/OuickSort/OuickSort.c
@@ -0,0 +1,65 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#define SIZE 10
+int sravn = 0, perest = 0;
+//Сортировка 
+void quickSort(double* arr, int first, int last)
+{
+    if (first < last)
+    {
+        int left = first, right = last;
+        double medium = arr[(left + right) / 2];
+        do
+        {
+            while (arr[left] < medium)
+                left++;
+            sravn++;
+                while (arr[right] > medium)
+                right--;
+                sravn++;
+                if (left <= right)
+                    sravn++;
+                  
+            {
+                double tmp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = tmp;
+                left++;
+                right--;
+                perest++;
+
+            }
+        } while (left <= right);
+        
+        quickSort(arr, first, right);
+        quickSort(arr, left, last);
+    }
+}
+
+int main()
+{
+    double a[SIZE];
+    
+    printf("Massiv: \n");
+    // Вводим значения элементов массива
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("arr[%d] = ", i);
+        scanf("%lf", &a[i]);
+    }
+    printf("\n Sorted array: \n");
+   
+    quickSort(a, 0, SIZE - 1); // Вызов функции сортировки
+              // Вывод элементов массива после сортировки
+    for (int i = 0; i < SIZE; i++) 
+    {
+        printf ("%.5lf \n", a[i]);
+    }
+    printf("\n Number of sravneny = %d", sravn);
+    printf("\n Number of perestanovok = %d", perest);
+    return 0;
+}
+
+
