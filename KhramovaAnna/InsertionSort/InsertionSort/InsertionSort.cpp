52c101c0175356f7fd50275849e277ed13c615c6
annakhramovaa <anahramova0@gmail.com>
2021-12-19 14:31:33 +0300

    InsertionSort

--- KhramovaAnna/InsertionSort/InsertionSort/InsertionSort.cpp
+++ KhramovaAnna/InsertionSort/InsertionSort/InsertionSort.cpp
@@ -1,37 +1,63 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
-int main() {
-	const int size = 5;
-	double array[size];
+int swap = 0;
+int comp = 0;
+
+void InsertionSort(double array[], int size) {
 	int i = 0;
-	int comp = 0, swap = 0;
-	srand(0);
-	for (i = 0; i < size; i++) {
-		array[i] = rand();
-		printf("a[%d] = %.1lf\t\n", i, array[i]);
-	}
-	/*for (i = 0; i < size; i++) {
-		printf("a[%d] = ", i);
-		scanf_s("%lf", &array[i]);
-	}*/
-	printf("\nsorted massive\n");
+
 	for (int i = 1; i < size; i++) {
 		comp = comp + 1;
 		for (int j = i; j > 0 && array[j - 1] > array[j]; j--) {
+
 			double tmp = array[j - 1];
 			array[j - 1] = array[j];
 			array[j] = tmp;
+			swap = swap + 3;
 			comp = comp + 2;
-			swap = swap + 1;
 		}
-		printf("the number of comparisons = %d\n", comp);
-		printf("the number of swaps = %d\n", swap);
 	}
+}
+void check_array(double array[], int size)
+{
+	for (int i = 1; i < size; i++)
+	{
+		if (array[i - 1] > array[i])
+		{
+			printf("\nSort is completed incorrectly\n");
+			break;
+		}
+		else
+		{
+			printf("\nSort is correctly completed\n");
+			break;
+		}
+	}
+}
+
+int main() {
+	const int size = 100;
+	double array[size];
+	int i = 0;
+	srand(777);
 
 	for (i = 0; i < size; i++) {
-		printf("a[%d]= %.1lf\t\n", i, array[i]);
+		printf("a[%d] = ", i);
+		array[i] = (double)(rand()) / RAND_MAX * (2000) - 1000;
+		printf("%lf\n", array[i]);
 	}
+
+	InsertionSort(array, size);
+	printf("\nSORTED MASSIVE\n");
+	for (i = 0; i < size; i++) {
+		printf("a[%d]= %lf\n", i, array[i]);
+	}
+
+	printf("CHEKING THE CORRECTNESS OF SORTING:\n");
+	check_array(array, size);
+
 	printf("the number of comparisons = %d\n", comp);
-	printf("the number of swaps = %d", swap);
+	printf("the number of swaps = %d\n", swap);
+
 	return 0;
 }
