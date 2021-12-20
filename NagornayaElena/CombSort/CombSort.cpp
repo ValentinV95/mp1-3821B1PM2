8ef40c602af5ee5b19ff0275f6a144a7c2d6f232
LoveCheeks <nagornayaelenaal@gmail.com>
2021-12-20 18:59:07 +0300

    Laba

--- NagornayaElena/CombSort/CombSort.cpp
+++ NagornayaElena/CombSort/CombSort.cpp
@@ -1,22 +1,25 @@
-﻿#include <iostream> #include <stdio.h> #include <math.h>
+﻿#include <iostream> 
+#include <stdio.h> 
+#include <math.h>
 
 int main()
 {
-	int i;
+	int i,j, k = 0;
 	const int n = 30;
-	float A[n], B[n];
-	srand(777);
-	for (i = 0; i < n; i++) {
-		                    A[i] = rand() % 256;
-							B[i] = A[i];
-		                    printf("%.1f  ", A[i]);
-	                        }
+	float A[n], B[n], tmp;
 	double factor = 1.2473309;
 	int step = n - 1;
+	srand(1000);
+	for (i = 0; i < n; i++) {
+		                    A[i] = -100+rand() % 1000;
+							B[i] = A[i];
+		                    printf("%.0f  ", A[i]);
+	                        }
+
 	while (step >= 1) {
 		              for (i = 0; i + step < n; i++) {
 						                             if (A[i] > A[i + step]) {
-														                      float tmp = A[i];
+														                      tmp = A[i];
 																			  A[i] = A[i + step];
 																			  A[i + step] = tmp;
 													                          }
@@ -24,22 +27,19 @@ int main()
 					  step /= factor;
 	                   }
 	printf("\n");
-	for (i = 0; i < n; i++) { printf("%.1f  ", A[i]); }
+	for (i = 0; i < n; i++) { printf("%.0f  ", A[i]); }
 
-	printf("\n");
-	for (i = 0; i < n; i++) { printf("%.1f  ", A[i]); }
 	for (i = 0; i < (n - 1); i++)
 		for (j = n - 2; j > (i - 1); j--)
 			if (B[j] > B[j + 1]) {
-				float tmp = B[j];
+				tmp = B[j];
 				B[j] = B[j + 1];
 				B[j + 1] = tmp;
 			}
-	int k = 0;
 	printf("\n");
 	for (i = 0; i < n; i++) {
 		if (A[i] == B[i]) { k = k + 1; };
-		printf("%.1f  ", B[i]);
+		printf("%.0f  ", B[i]);
 	}
 	printf("\n");
 	if (k == n) { printf("Correct"); }
--- NagornayaElena/InsertionSort/InsertionSort.cpp
+++ NagornayaElena/InsertionSort/InsertionSort.cpp
@@ -1,40 +1,40 @@
-﻿#include <iostream> #include <stdio.h> #include <math.h>
+﻿#include <stdlib.h>
+#include <iostream> 
+#include <stdio.h> 
+#include <math.h>
 
-int main()
+void main()
 {
-	int i,j;
-	const int n = 10;
-	float A[n], B[n];
-	srand(800);
+	int i,j,k = 0;
+	const int n = 25;
+	float A[n], B[n], tmp;
+	srand(1000);
 	for (i = 0; i < n; i++) {
-		                    A[i] = rand()%256;
+		                    A[i] = -100+ rand()%1000;
 							B[i] = A[i];
-							printf("%.1f  ", A[i]);
+							printf("%.0f  ", A[i]);
                              }
 	for (i = 1; i < n; i++)  
 		for (j = i; (j > 0) && (A[j - 1] > A[j]); j--) {
-			                                           float tmp = A[j - 1];
+			                                           tmp = A[j - 1];
 			                                           A[j - 1] = A[j];
 			                                           A[j] = tmp;
 		                                               }
 	printf("\n");
-	for (i = 0; i < n; i++) { printf("%.1f  ", A[i]); }
-	for (i=0; i<(n-1); i++) 
+	for (i = 0; i < n; i++) { printf("%.0f  ", A[i]); }
+	printf("\n");
+	for (i = 0; i < n-1; i++) 
 		for (j=n-2; j>(i-1);j--)
 			if (B[j] > B[j + 1]) {
-				                 float tmp = B[j];
+				                 tmp = B[j];
 								 B[j] = B[j + 1];
 								 B[j + 1] = tmp;
 								 }
-	int k = 0;
-	printf("\n");
 	for (i = 0; i < n; i++) { 
 		                     if (A[i] == B[i]) { k = k + 1; };
-	                       	 printf("%.1f  ", B[i]); 
+	                       	 printf("%.0f  ", B[i]); 
 	                         }
-	printf("\n");
+	printf("\n ");
 	if (k == n) { printf("Correct"); }
 	else {printf("No correct"); }
-
-	return 0;
 }
