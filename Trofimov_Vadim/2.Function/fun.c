c9f45ab35c8ff036545bf8d091afadd9966eb824
Sir Truber <mega.boss.vadim@mail.ru>
2022-03-20 23:21:53 +0300

    added a new code for calculating pi const and sincos metod

--- Trofimov_Vadim/2.Function/fun.c
+++ Trofimov_Vadim/2.Function/fun.c
@@ -27,6 +27,18 @@ void cross(int n, double* a){
         i+=2;
     }
 }
+//функция вычисления числа Пи/6 через ряд Маклорена для арксинуса. Сходится при 504 элементах.
+double pix(){
+    double* a = (double*)malloc(504 * sizeof(double));
+    int n = 1;
+    a[0] = 0.5;
+    for(;a[n-1] > DBL_MIN;++n){
+		a[n] =a[n - 1]*(a[0]*a[0]+(1-6*n)/(2.0*(2*n*n+n))*a[0]*a[0]);
+    }
+    rev(n,a);
+    return(a[0]);
+    free(a);
+}
 
 //функции вычисления ряда Маклорена. На вход принимает аргумент
 //вычисляемой функции(для тригонометрических функций в радианах).
@@ -94,9 +106,51 @@ double cosx(double x){
         default:
             printf("Invalid operation");
             exit(0);
-    }
-    return(a[0]);
+    }return(a[0]);
     free(a);
+}
+
+//функция, подсчета тригонометрических функций путем формул приведения. Работает за меньшее кол-во элементов, но требует больше проверок
+double sinxcosx(double x){
+    int a = floor(x/(3*pix())+0.5),n;
+    double res;
+    printf("1.sin\n2.cos\n");
+    scanf("%d",&n);
+    res = x - 3*a*pix();
+    a%=4;
+    if (a<0){
+        a+=4;
+        a%=4;
+    }
+    switch(n){
+        case 1:
+            if(a ==1)
+                res=cosx(res);
+            else if(a==3)
+                res=-cos(res);
+            else if(a==2)
+                res=-sinx(res);
+            else
+                res=sinx(res);
+            printf("res = %.16lf\nsin = %.16lf\nabs err=%.16lf\nrel err=%.16lf\n",res,sin(x),abs(res-sin(x)),abs(1.-res/sin(x)));
+            break;
+        case 2:
+            if(a ==1)
+                res=sinx(res);
+            else if(a==3)
+                res=-sin(res);
+            else if(a==2)
+                res=-cosx(res);
+            else
+                res=cosx(res);
+            printf("res = %.16lf\ncos = %.16lf\nabs err=%.16lf\nrel err=%.16lf\n",res,cos(x),abs(res-cos(x)),abs(1.-res/cos(x)));
+            break;
+        default:
+            printf("Invalid operation");
+            exit(0);
+            break;
+    }
+return(res);
 }
 
 double expx(double x){
@@ -144,8 +198,8 @@ double lnx(double x){
         a = (double*)realloc(a,(n+2) * sizeof(double));
     }
     printf("number %d",n);
-    printf("Select a type of sum\n1.Dirrect\n2.Reverse\n");
-    scanf("%d",&choise);
+    //printf("Select a type of sum\n1.Dirrect\n2.Reverse\n");
+    //scanf("%d",&choise);
     switch (choise){
         case 1:
             dir(n,a);
@@ -164,14 +218,10 @@ double lnx(double x){
 int main(){
     int variant;
     double x,res;
-    FILE * rs = fopen("/home/home/Code/2.Function/result.txt","w");
-    FILE * rf = fopen("/home/home/Code/2.Function/reference.txt","w");
-    FILE * ab = fopen("/home/home/Code/2.Function/abs.txt","w");
-    FILE * rel = fopen("/home/home/Code/2.Function/relative.txt","w");
-    if (rs==NULL|| rf==NULL|| ab==NULL||rel==NULL)
-        exit(0);
+    FILE * rs = fopen("/home/home/Code/2.Function/result.txt","w");FILE * rf = fopen("/home/home/Code/2.Function/reference.txt","w");FILE * ab = fopen("/home/home/Code/2.Function/abs.txt","w");FILE * rel = fopen("/home/home/Code/2.Function/relative.txt","w");
+    if (rs==NULL|| rf==NULL|| ab==NULL||rel==NULL) exit(0);
     for(;;){
-        printf("Select a operation:\n1.sin\n2.cos\n3.exp\n4.log\n0.exit\n");
+        printf("Select a operation:\n1.sin\n2.cos\n3.sincos\n4.exp\n5.log\n0.exit\n");
         scanf("%d",&variant);
         if(variant==0)
             exit(0);
@@ -181,22 +231,22 @@ int main(){
             case 1:
                 for(x=-10;x<10;x+=0.1){
                 res = sinx(x);
-                fprintf(rs,"%.1lf %.16lf\n",x,res);
-                fprintf(rf,"%.1lf %.16lf\n",x,sin(x));
-                fprintf(ab,"(%.1lf) (%.16lf)\n",x,abs(res-sin(x)));
-                fprintf(rel,"%.1lf %.16lf\n",x,abs(1-sinx(x)/sin(x)));
-                }
-                fclose(rs),fclose(rf),fclose(ab),fclose(rel);
+                fprintf(rs,"%.1lf %.16lf\n",x,res);fprintf(rf,"%.1lf %.16lf\n",x,sin(x));fprintf(ab,"(%.1lf) (%.16lf)\n",x,abs(res-sin(x)));fprintf(rel,"%.1lf %.16lf\n",x,abs(1-sinx(x)/sin(x)));
+                }fclose(rs),fclose(rf),fclose(ab),fclose(rel);
+                //printf("res = %.16lf\nsin = %.16lf\nabs err=%.16lf\nrel err=%.16lf\n",res,sin(x),abs(res-sin(x)),abs(1.-res/sin(x)));
                 break;
             case 2:
                 res = cosx(x);
                 printf("res = %.16lf\ncos = %.16lf\nabs err=%.16lf\nrel err=%.16lf\n",res,cos(x),abs(res-cos(x)),abs(1.-res/cos(x)));
                 break;
             case 3:
+                res = sinxcosx(x);
+                break;
+            case 4:
                 res = expx(x);
                 printf("res = %.16lf\nexp = %.16lf\nabs err=%.16lf\nrel err=%.16lf\n",res,exp(x),abs(res-exp(x)),abs(1.-res/exp(x)));
                 break;
-            case 4:
+            case 5:
                 res = lnx(x);
                 printf("res = %.16lf\nlog = %.16lf\nabs err=%.16lf\nrel err=%.16lf\n",res,log(x),abs(res-log(x)),abs(1.-res/log(x)));
                 break;
