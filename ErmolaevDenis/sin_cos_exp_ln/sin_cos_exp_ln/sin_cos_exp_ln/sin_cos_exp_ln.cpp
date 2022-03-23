95193d5799c49ee72a0b979f5ead77798fa02394
ErmolaevDenis <den.ermolaev.2003@mail.ru>
2022-03-23 23:26:20 +0300

    исправлен код

--- ErmolaevDenis/sin_cos_exp_ln/sin_cos_exp_ln/sin_cos_exp_ln/sin_cos_exp_ln.cpp
+++ ErmolaevDenis/sin_cos_exp_ln/sin_cos_exp_ln/sin_cos_exp_ln/sin_cos_exp_ln.cpp
@@ -5,7 +5,7 @@
 #include "string.h"
 #include "stdlib.h"
 
-double next(char* namefunc, double pre, int i, double x)
+float next(char* namefunc, float pre, int i, float x)
 {
     if (strcmp(namefunc, "sin") == 0) return -1.0 * pre * x * x / (2.0 * i * (2.0 * i + 1.0));
     if (strcmp(namefunc, "cos") == 0) return -1.0 * pre * x * x / ((i * 2.0 - 1.0) * (i * 2.0));
@@ -17,16 +17,16 @@ double next(char* namefunc, double pre, int i, double x)
     }
 }
 
-double first(char* namefunc, double x)
+float first(char* namefunc, float x)
 {
     if ((strcmp(namefunc, "sin") == 0) || (strcmp(namefunc, "ln") == 0)) return x;
-    if ((strcmp(namefunc, "cos") == 0) || (strcmp(namefunc, "exp") == 0)) return 1;
+    if ((strcmp(namefunc, "cos") == 0) || (strcmp(namefunc, "exp") == 0)) return 1.0;
 }
 
-double directsum(char* namefunc, double x)
+float directsum(char* namefunc, float x)
 {
     int i = 0;
-    double pre = 0, func = 0;
+    float pre = 0, func = 0;
     pre = first(namefunc, x);
     func = first(namefunc, x);
     for (i = 1; i < 101; i++)
@@ -37,10 +37,10 @@ double directsum(char* namefunc, double x)
     return func;
 }
 
-double reversesum(char* namefunc, double x)
+float reversesum(char* namefunc, float x)
 {
     int i = 0;
-    double mas[100], last = 0, func = 0;
+    float mas[100], last = 0, func = 0;
     last = first(namefunc, x);
     for (i = 1; i < 101; i++)
     {
@@ -54,53 +54,52 @@ double reversesum(char* namefunc, double x)
     return func;
 }
 
-double pairwisesum(char* namefunc, double x)
+float pairwisesum(char* namefunc, float x)
 {
     int i = 0;
-    double func = 0, func1 = 0, func2 = 0, last = 0, mas[100];
+    float func = 0, last = 0, mas[100];
     last = first(namefunc, x);
     for (i = 1; i < 101; i++)
     {
         mas[i - 1] = last;
         last = next(namefunc, last, i, x);
     }
-    for (i = 1; i < 101; i++)
+    for (i = 2; i < 101; i += 2)
     {
-        if ((i - 1) % 2 == 0) func1 += mas[i - 1];
-        else func2 += mas[i - 1];
+        func += mas[i - 2] + mas[i - 1];
     }
-    func = func1 + func2;
     return func;
 }
 
-void printfunc(char* namefunc, double func, double x)
+void printfunc(char* namefunc, float func, float x)
 {
     
-    if (sin(x) != 0 || cos(x) != 0 || log(x + 1) != 0 || exp(x) != 0)
+    if (sin(x) != 0 || cos(x) != 0 || log(x + 1.0) != 0 || exp(x) != 0)
     {
-        if (strcmp(namefunc, "sin") == 0) printf("\nX = %lf My func = %.16lf Absolute error = %.16lf Relative error = %.16lf\n", x, func, fabs(func - sin(x)), (fabs(sin(x) - func) / fabs(sin(x))) * 100);
-        if (strcmp(namefunc, "cos") == 0) printf("\nX = %lf My func = %.16lf Absolute error = %.16lf Relative error = %.16lf\n", x, func, fabs(func - cos(x)), (fabs(cos(x) - func) / fabs(cos(x))) * 100);
-        if (strcmp(namefunc, "exp") == 0) printf("\nX = %lf My func = %.16lf Absolute error = %.16lf Relative error = %.16lf\n", x, func, fabs(func - exp(x)), (fabs(exp(x) - func) / fabs(exp(x))) * 100);
-        if (strcmp(namefunc, "ln") == 0) printf("\nX = %lf My func = %.16lf Absolute error = %.16lf Relative error = %.16lf\n", x, func, fabs(func - log(x + 1)), (fabs(log(x + 1) - func) / fabs(log(x + 1))) * 100);
+        if (strcmp(namefunc, "sin") == 0) printf("\nX = %f My func = %.8f Absolute error = %.8f Relative error = %.8f%%\n", x, func, fabsf(func - sin(x)), (fabsf(sin(x) - func) / fabsf(sin(x))) * 100.0);
+        if (strcmp(namefunc, "cos") == 0) printf("\nX = %f My func = %.8f Absolute error = %.8f Relative error = %.8f%%\n", x, func, fabsf(func - cos(x)), (fabsf(cos(x) - func) / fabsf(cos(x))) * 100.0);
+        if (strcmp(namefunc, "exp") == 0) printf("\nX = %f My func = %.8f Absolute error = %.8f Relative error = %.8f%%\n", x, func, fabsf(func - exp(x)), (fabsf(exp(x) - func) / fabsf(exp(x))) * 100.0);
+        if (strcmp(namefunc, "ln") == 0) printf("\nX = %f My func = %.8f Absolute error = %.8f Relative error = %.8f%%\n", x, func, fabsf(func - log(x + 1.0)), (fabsf(log(x + 1.0) - func) / fabsf(log(x + 1.0))) * 100.0);
     }
     else
     {
-        if (strcmp(namefunc, "sin") == 0) printf("\nX = %lf My func = %.16lf Absolute error = %.16lf Relative error = 0.0000000000000000\n", x, func, fabs(func - sin(x)));
-        if (strcmp(namefunc, "cos") == 0) printf("\nX = %lf My func = %.16lf Absolute error = %.16lf Relative error = 0.0000000000000000\n", x, func, fabs(func - cos(x)));
-        if (strcmp(namefunc, "exp") == 0) printf("\nX = %lf My func = %.16lf Absolute error = %.16lf Relative error = 0.0000000000000000\n", x, func, fabs(func - exp(x)));
-        if (strcmp(namefunc, "ln") == 0) printf("\nX = %lf My func = %.16lf Absolute error = %.16lf Relative error = 0.0000000000000000\n", x, func, fabs(func - log(x + 1)));
+        if (strcmp(namefunc, "sin") == 0) printf("\nX = %f My func = %.8f Absolute error = %.8f Relative error = 0.00000000%\n", x, func, fabsf(func - sin(x)));
+        if (strcmp(namefunc, "cos") == 0) printf("\nX = %f My func = %.8f Absolute error = %.8f Relative error = 0.00000000%\n", x, func, fabsf(func - cos(x)));
+        if (strcmp(namefunc, "exp") == 0) printf("\nX = %f My func = %.8f Absolute error = %.8f Relative error = 0.00000000%\n", x, func, fabsf(func - exp(x)));
+        if (strcmp(namefunc, "ln") == 0) printf("\nX = %f My func = %.8f Absolute error = %.8f Relative error = 0.00000000%\n", x, func, fabsf(func - log(x + 1.0)));
     }
 }
 
 int main()
 {
     char namefunc[4];
-    double x = 0, func = 0;
+    float x = 0.0, func = 0.0;
     int i, type;
     printf("Enter the function name (sin, cos, exp, ln) : ");
     scanf("%s", namefunc);
+    if (strcmp(namefunc, "sin") != 0 && strcmp(namefunc, "ln") != 0 && strcmp(namefunc, "cos") != 0 && strcmp(namefunc, "exp") != 0) { printf("ERROR"); return 0; }
     printf("\nEnter x : ");
-    scanf_s("%lf", &x);
+    scanf_s("%f", &x);
     if ((strcmp(namefunc, "ln") == 0) && (x >= 1 || x <= -1)) { printf("ERROR: x must be in the range (-1;1)"); return 0; }
     printf("Select the type of summation:\n1 - direct summation \n2 - reverse summation\n3 - pairwise summation\n");
     scanf_s("\n%i", &type);
