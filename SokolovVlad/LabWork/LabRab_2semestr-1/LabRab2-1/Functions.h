3abf96f20cd9672f1362594ee2de9a21c945e675
Sokolov-Vladisl <elifantastika1.com>
2022-03-24 15:05:27 +0300

    Код модифицирован

--- SokolovVlad/LabWork/LabRab_2semestr-1/LabRab2-1/Functions.h
+++ SokolovVlad/LabWork/LabRab_2semestr-1/LabRab2-1/Functions.h
@@ -7,7 +7,7 @@
 #include <float.h>
 #include <math.h>
 
-#ifdef MYDLL_EXPORTS
+#ifdef MYDLL_EXPORTS                            //подстраховка
 #define MYDLL_API __declspec(dllexport)
 #else
 #define MYDLL_API __declspec(dllimport)
@@ -15,50 +15,53 @@
 
 /* --------------------------------------------------------------------------------------------
 
-          ФОРМУЛЫ НАХОЖДЕНИЯ синуса, косинуса, экспоненты, натурального логарифма(1+x)
+		  ФОРМУЛЫ НАХОЖДЕНИЯ синуса, косинуса, экспоненты, натурального логарифма(1+x)
 
- sinx = x - x^3/3! + x^5/5! - ... + (-1)^m * x^(2m+1)/(2m+1)!      
+ sinx = x - x^3/3! + x^5/5! - ... + (-1)^m * x^(2m+1)/(2m+1)!
 
- cosx = 1 - x^2/2! + x^4/4! - ... + (-1)^n * x^2n/(2n)!          
+ cosx = 1 - x^2/2! + x^4/4! - ... + (-1)^n * x^2n/(2n)!
 
  expx = 1 + x + x^2/2! + ... + x^k/k!                  k - любая
 
- ln(1+x) = x - x^2/2 + x^3/3 - ... + (-1)^(n-1) * x^n/n        
+ ln(1+x) = x - x^2/2 + x^3/3 - ... + (-1)^(n-1) * x^n/n
 
-  -------------------------------------------------------------------------------------------- 
+  --------------------------------------------------------------------------------------------
   */
 
 
+ //--------------------------------------------------------------------------------------------
+
+
+float next_element_sin(float prevElement, float x, int i)
+{
+	float nes = (prevElement * x * x) / ((2.0 * i) * (2.0 * i + 1.0));
+	if (nes > FLT_EPSILON)
+		return nes;
+	else return 0.0;
+}
+
+float next_element_lnx(float prevElement, float x, int i)
+{
+	return i * prevElement * x / (i + 1.0);
+}
+
+float next_element_cos(float prevElement, float x, int i)
+{
+	return prevElement * x * x / (2.0 * i - 1) / (2.0 * i);
+}
+
+float next_element_exp(float prevElement, float x, int i)
+{
+	return prevElement * x / (i + 0.0);
+}
+
+
 //--------------------------------------------------------------------------------------------
 
 
-double next_element_sin(double predEl, double x, int i)
+float recursive_sin(float x, float xconst, int i)
 {
-	return predEl * x * x / ((2.0 * i) * (2.0 * i + 1.0));
-}
-
-double next_element_lnx(double predEl, double x, int i)
-{
-	return i * predEl * x / (i + 1.0);
-}
-
-double next_element_cos(double predEl, double x, int i)
-{
-	return predEl * x * x / (2.0 * i - 1) / (2.0 * i);
-}
-
-double next_element_exp(double predEl, double x, int i)
-{
-	return predEl * x / (i + 0.0);
-}
-
-
-//--------------------------------------------------------------------------------------------
-
-
-double recursive_sin(double x, double xconst, int i)
-{
-	if (x > DBL_EPSILON)
+	if (x > FLT_EPSILON)
 	{
 		if (i % 2 != 0)
 			return -x + recursive_sin(x * xconst * xconst / ((2.0 * (i + 1.0)) * (2.0 * (i + 1.0) + 1)), xconst, i + 1);
@@ -68,9 +71,9 @@ double recursive_sin(double x, double xconst, int i)
 	else return x;
 }
 
-double recursive_cos(double x, double xconst, int i)
+float recursive_cos(float x, float xconst, int i)
 {
-	if (x > DBL_EPSILON)
+	if (x > FLT_EPSILON)
 	{
 		if (i % 2 != 0)
 			return -x + recursive_cos(x * xconst * xconst / ((2.0 * (i + 1.0)) * (2.0 * (i + 1.0) - 1)), xconst, i + 1);
@@ -80,18 +83,18 @@ double recursive_cos(double x, double xconst, int i)
 	else return x;
 }
 
-double recursive_exp(double x, double xconst, int i)
+float recursive_exp(float x, float xconst, int i)
 {
-	if (x > DBL_EPSILON && i < 4100)                                        // Если будет больше 4161, то ОШИБКА!
+	if (x > FLT_EPSILON && i < 4000)                                        // Если будет больше 4161, то ОШИБКА!
 		return x + recursive_exp(x * xconst / (i + 1.0), xconst, i + 1);
 	else return x;
 }
 
-double recursive_ln(double x, double xconst, int i)
+float recursive_ln(float x, float xconst, int i)
 {
-	if (x <= 1 && x > -1)
+	if ((x <= 1 && x > -1) && (i < 4000 && xconst != 1))
 	{
-		if (x > DBL_EPSILON)
+		if (x > FLT_EPSILON)
 		{
 			if (i % 2 == 0)
 				return -x + recursive_ln(x * xconst * i / (i + 1.0), xconst, i + 1);
