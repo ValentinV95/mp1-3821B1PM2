22e9550d6907bffeabd225d14ebb5a73d5fe899e
LogiNNegative <95866485+LogiNNegative@users.noreply.github.com>
2022-03-30 01:08:48 +0300

    Add files via upload

---
+++ LoginovSergey/LabWork/LabWork/LabWork.c
@@ -0,0 +1,148 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define SummType SimpleSumm(array, n);  
+//#define SummType ReverseSumm(array, n);
+//#define SummType PairwiseSumm (array, n); 
+
+float SinNext(float prev, float x, int i)
+{
+	i = 2 * i + 1;
+	return (prev * (-1.f) * x * x) / (i * (i - 1.f));
+}
+
+float CosNext(float prev, float x, int i)
+{
+	i = 2 * i;
+	return (prev * (-1.f) * x * x) / (i * (i - 1.f));
+}
+
+float ExpNext(float prev, float x, int i)
+{
+	return (prev * x) / ((float)i);
+}
+
+float LnNext(float prev, float x, int i)
+{
+	return (prev * (-1.f) * x * i) / (i + 1.f);
+}
+
+float SimpleSumm(float* array, int n)
+{
+	float summ = 0;
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		summ += array[i];
+	}
+	return summ;
+}
+
+float ReverseSumm(float* array, int n)
+{
+	float summ = 0;
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		summ += array[n - i];
+	}
+	return summ;
+}
+
+float PairwiseSumm(float* array, int n)
+{
+	int i;
+	float summ = 0;
+
+	for (i = 0; i < n - 1; i += 2)
+		array[i] += array[i + 1];
+
+	if (n % 2 == 1)
+		summ += array[i + 2];
+
+	for (i = 0; i < n; i += 2)
+		summ += array[i];
+
+	return summ;
+}
+
+void CorrectCheck(float res, double(*original)(double x), double x)
+{
+	printf("\nAbsolute error is: %.8f", fabsf((float)original(x) - res));
+	printf("\nRelative error is: %.8f %%", fabsf((((float)original(x) - res) / (float)original(x)) * 100.f));
+}
+
+void ArrayFill(float* array, int n, float(*res)(float, float, int), float param)
+{
+	int i;
+
+	for (i = 1; i < n; i++)
+	{
+		array[i] = res(array[i - 1], param, i);
+	}
+}
+
+void Menu(float* array, int n)
+{
+	int choice;
+	double x;
+	float summ = 0;
+
+	printf("Choose function:\n");
+	printf("Enter:\n 1 for sin(x)\n 2 for cos(x)\n 3 for exp(x)\n 4 for ln(1+x)\n");
+	scanf_s("%d", &choice);
+	printf("Enter x:\n");
+	scanf_s("%lf", &x);
+
+
+	switch (choice)
+	{
+	case(1):
+
+		array[0] = (float)x;
+		ArrayFill(array, n, SinNext, x);
+		summ = SummType;
+		printf("\nsin(%.8f) = %.8f", x, summ);
+		CorrectCheck(summ, sin, x);
+		break;
+
+	case(2):
+
+		array[0] = 1.f;
+		ArrayFill(array, n, CosNext, x);
+		summ = SummType;
+		printf("\ncos(%lf) = %.16lf", x, summ);
+		CorrectCheck(summ, cos, x);
+		break;
+
+	case(3):
+
+		array[0] = 1.f;
+		ArrayFill(array, n, ExpNext, x);
+		summ = SummType;
+		printf("\nexp(%lf) = %.16lf", x, summ);
+		CorrectCheck(summ, exp, x);
+		break;
+
+	case(4):
+
+		array[0] = (float)x;
+		ArrayFill(array, n, LnNext, x);
+		summ = SummType;
+		printf("\nln(1 + %lf) = %.16lf", x, summ);
+		CorrectCheck(summ, log, x + 1);
+		break;
+	}
+}
+
+void main()
+{
+	float* array;
+	int N = 100;
+
+	array = (float*)malloc(N * sizeof(float));
+	Menu(array, N);
+}
+
