f56964d51c998bbcc8b493e612ddc1f98c226e19
JuliaAnisimovaa <94898957+JuliaAnisimovaa@users.noreply.github.com>
2022-04-02 17:24:57 +0300

    Update Laba_2.c

--- Laba_2_sin_cos_exp_ln/Laba_2/Laba_2.c
+++ Laba_2_sin_cos_exp_ln/Laba_2/Laba_2.c
@@ -2,12 +2,12 @@
 #include <locale.h>
 #include <stdlib.h>
 #include<math.h>
-#define amount_type direct_summ (arr,n);
-typedef double(*option)(double, double, int);
+#define amount_type revers_summ (arr,n);
+typedef float(*option)(float, float, int);
 
-double direct_summ(double* arr, int m)
+float direct_summ(float* arr, int m)
 {
-	double summ = 0;
+	float summ = 0;
 	int i;
 	for (i = 0; i < m; i++)
 	{
@@ -15,9 +15,9 @@ double direct_summ(double* arr, int m)
 	}
 	return summ;
 }
-double revers_summ(double* arr, int m)
+float revers_summ(float* arr, int m)
 {
-	double summ = 0;
+	float summ = 0;
 	int i;
 	for (i = 1; i <= m; i++)
 	{
@@ -25,38 +25,37 @@ double revers_summ(double* arr, int m)
 	}
 	return summ;
 }
-double paired_summ(double* arr, int m)
+float paired_summ(float* arr, int m)
 {
-	int i, j;
-	while (m > 1)
-	{
-		for (i = 1, j = 0; i < m; i += 2, ++j)
-		{
-			arr[j] = arr[i] + arr[i - 1];
-		}
-		m /= 2;
-	}
-	return arr[0];
+	int i;
+	float summ = 0;
+	for (i = 0; i < m - 1; i += 2)
+		arr[i] += arr[i + 1];
+	if (m % 2 == 1)
+		summ += arr[i + 2];
+	for (i = 0; i < m; i += 2)
+		summ += arr[i];
+	return summ;
 }
-double cos_1(double pred, double n, int i)
+float cos_1(float pred, float n, int i)
 {
 	i = 2 * i;
-	return (pred * (-1.0) * n * n) / (i * (i - 1.0));
+	return (pred * (-1.f) * n * n) / (i * (i - 1.f));
 }
-double sin_1(double pred, double n, int i)
+float sin_1(float pred, float n, int i)
 {
 	i = 2 * i + 1;
-	return (pred * (-1.0) * n * n) / (i * (i - 1.0));
+	return (pred * (-1.f) * n * n) / (i * (i - 1.f));
 }
-double ln_1(double pred, double n, int i)
+float ln_1(float pred, float n, int i)
 {
-	return (pred * (-1.0) * n * i) / (i + 1.0);
+	return (pred * (-1.f) * n * i) / (i + 1.f);
 }
-double exp_1(double pred, double n, int i)
+float exp_1(float pred, float n, int i)
 {
-	return (pred * n) / ((double)i);
+	return (pred * n) / ((float)i);
 }
-void made_array(double* arr, int m, option result, double p)
+void made_array(float* arr, int m, option result, float p)
 {
 	int i;
 
@@ -66,61 +65,61 @@ void made_array(double* arr, int m, option result, double p)
 	}
 }
 
-int main()
+void main()
 {
-	double* arr = NULL;
-	double x;
+	float* arr;
+	float x;
 	int select;
 	int n = 100;
-	double summ = 0;
+	float summ = 0;
 	setlocale(LC_ALL, "Rus");
 	printf("Выберите функцию:\n");
 	printf("1) косинус\n2) синус\n3) натуральный логарифм от (1+x)\n4) экспонента\n");
 	scanf_s("%d", &select);
 	printf("Введите x:\n");
-	scanf_s("%lf", &x);
-	arr = (double*)malloc(n * sizeof(double));
+	scanf_s("%f", &x);
+	arr = (float*)malloc(n * sizeof(float));
 	switch (select)
 	{
 	case(1):
 	{
-		arr[0] = 1;
-		made_array(arr, n, cos_1, x);
+		arr[0] = 1.f;
+		made_array(arr, n, cos_1, (float)x);
 		summ = amount_type;
-		printf("\ncos(x) = %.15lf", summ);
-		printf("\nЗначение из библиотеки: %lf\n", cos(x));
-		printf("\nПогрешность: %.15lf", (cos(x) - summ));
-		printf("\nОтносительная погрешность в процентах: %.15lf ", (((cos(x) - summ) / summ) * 100));
+		printf("\ncos(x) = %.8f", summ);
+		printf("\nЗначение из библиотеки: %f\n", (float)cos(x));
+		printf("\nПогрешность: %.8f", (cos(x) - summ));
+		printf("\nОтносительная погрешность в процентах: %.8f ", (((cos(x) - summ) / cos(x)) * 100));
 		break;
 
 	case(2):
-		arr[0] = x;
-		made_array(arr, n, sin_1, x);
+		arr[0] = (float)x;
+		made_array(arr, n, sin_1, (float)x);
 		summ = amount_type;
-		printf("\nsin(x) = %.15lf", summ);
-		printf("\nЗначение из библиотеки: %lf\n", sin(x));
-		printf("\nПогрешность: %.15lf", (sin(x) - summ));
-		printf("\nОтносительная погрешность в процентах: %.15lf ", (((sin(x) - summ) / summ) * 100));
+		printf("\nsin(x) = %.8f", summ);
+		printf("\nЗначение из библиотеки: %f\n", (float)sin(x));
+		printf("\nПогрешность: %.8f", (sin(x) - summ));
+		printf("\nОтносительная погрешность в процентах: %.8f ", (((sin(x) - summ) / sin(x)) * 100));
 		break;
 
 	case(3):
-		arr[0] = x;
-		made_array(arr, n, ln_1, x);
+		arr[0] = (float)x;
+		made_array(arr, n, ln_1, (float)x);
 		summ = amount_type;
-		printf("\nln(1+x) = %.15lf", summ);
-		printf("\nЗначение из библиотеки: %lf\n", log(1 + x));
-		printf("\nПогрешность: %.15lf", (log(1 + x) - summ));
-		printf("\nОтносительная погрешность в процентах: %.15lf ", (((log(1 + x) - summ) / summ) * 100));
+		printf("\nln(1+x) = %.8f", summ);
+		printf("\nЗначение из библиотеки: %f\n", log(1.0 + x));
+		printf("\nПогрешность: %.8f", (log(1.0 + x) - summ));
+		printf("\nОтносительная погрешность в процентах: %.8f ", (((log(1.0 + x) - summ) / log(1.0 + x)) * 100));
 		break;
 
 	case(4):
-		arr[0] = 1;
-		made_array(arr, n, exp_1, x);
+		arr[0] = 1.f;
+		made_array(arr, n, exp_1, (float)x);
 		summ = amount_type;
-		printf("\nexp(x) = %.15lf", summ);
-		printf("\nЗначение из библиотеки: %lf\n", exp(x));
-		printf("\nПогрешность: %.15lf", (exp(x) - summ));
-		printf("\nОтносительная погрешность в процентах: %.15lf ", (((exp(x) - summ) / summ) * 100));
+		printf("\nexp(x) = %.8f", summ);
+		printf("\nЗначение из библиотеки: %f\n", exp(x));
+		printf("\nПогрешность: %.8f", (exp(x) - summ));
+		printf("\nОтносительная погрешность в процентах: %.8f ", (((exp(x) - summ) / exp(x)) * 100));
 		break;
 	}
 	default:
@@ -130,3 +129,5 @@ int main()
 	}
 	}
 }
+
+
