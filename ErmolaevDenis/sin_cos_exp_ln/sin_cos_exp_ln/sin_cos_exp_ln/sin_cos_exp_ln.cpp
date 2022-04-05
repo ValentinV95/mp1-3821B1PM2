4e5f2e5edb230a68e08a091e387aa316d3b4880d
ErmolaevDenis <den.ermolaev.2003@mail.ru>
2022-04-05 21:13:03 +0300

    fix

--- ErmolaevDenis/sin_cos_exp_ln/sin_cos_exp_ln/sin_cos_exp_ln/sin_cos_exp_ln.cpp
+++ ErmolaevDenis/sin_cos_exp_ln/sin_cos_exp_ln/sin_cos_exp_ln/sin_cos_exp_ln.cpp
@@ -5,48 +5,53 @@
 #include "string.h"
 #include "stdlib.h"
 
-float next(char* namefunc, float pre, int i, float x)
+
+float sin_next(float pre, int i, float x)
 {
-    if (strcmp(namefunc, "sin") == 0) return -1.0 * pre * x * x / (2.0 * i * (2.0 * i + 1.0));
-    if (strcmp(namefunc, "cos") == 0) return -1.0 * pre * x * x / ((i * 2.0 - 1.0) * (i * 2.0));
-    if (strcmp(namefunc, "ln") == 0) return -1.0 * pre * x * (i / (i + 1.0));
-    if (strcmp(namefunc, "exp") == 0)
-    {
+    return -1.0 * pre * x * x / (2.0 * i * (2.0 * i + 1.0));
+}
+
+float cos_next(float pre, int i, float x)
+{
+    return -1.0 * pre * x * x / ((i * 2.0 - 1.0) * (i * 2.0));
+}
+
+float ln_next(float pre, int i, float x)
+{
+    return -1.0 * pre * x * (i / (i + 1.0));
+}
+
+float exp_next(float pre, int i, float x)
+{
         if (i == 1) return x;
         else return pre * (x / (i));
-    }
 }
 
-float first(char* namefunc, float x)
+float sin_ln_first(float x)
 {
-    if ((strcmp(namefunc, "sin") == 0) || (strcmp(namefunc, "ln") == 0)) return x;
-    if ((strcmp(namefunc, "cos") == 0) || (strcmp(namefunc, "exp") == 0)) return 1.0;
+    return x;
 }
 
-float directsum(char* namefunc, float x)
+float cos_exp_first()
+{
+    return 1.0;
+}
+
+float directsum(float* mas)
 {
     int i = 0;
-    float pre = 0, func = 0;
-    pre = first(namefunc, x);
-    func = first(namefunc, x);
+    float func = 0;
     for (i = 1; i < 101; i++)
     {
-        func += next(namefunc, pre, i, x);
-        pre = next(namefunc, pre, i, x);
+        func += mas[i - 1];
     }
     return func;
 }
 
-float reversesum(char* namefunc, float x)
+float reversesum(float* mas)
 {
     int i = 0;
-    float mas[100], last = 0, func = 0;
-    last = first(namefunc, x);
-    for (i = 1; i < 101; i++)
-    {
-        mas[i - 1] = last;
-        last = next(namefunc, last, i, x);
-    }
+    float func = 0;
     for (i = 100; i > 0; i--)
     {
         func += mas[i - 1];
@@ -54,16 +59,10 @@ float reversesum(char* namefunc, float x)
     return func;
 }
 
-float pairwisesum(char* namefunc, float x)
+float pairwisesum(float* mas)
 {
     int i = 0;
-    float func = 0, last = 0, mas[100];
-    last = first(namefunc, x);
-    for (i = 1; i < 101; i++)
-    {
-        mas[i - 1] = last;
-        last = next(namefunc, last, i, x);
-    }
+    float func = 0;
     for (i = 2; i < 101; i += 2)
     {
         func += mas[i - 2] + mas[i - 1];
@@ -71,58 +70,177 @@ float pairwisesum(char* namefunc, float x)
     return func;
 }
 
-void printfunc(char* namefunc, float func, float x)
+void create_mas(float* mas, float(*next)(float, int, float), float x)
+{
+    int i;
+    for (i = 1; i < 100; i++)
+    {
+        mas[i] = next(mas[i - 1], i, x);
+    }
+}
+
+void printfunc(float func, double original, float x)
 {
     
     if (sin(x) != 0 || cos(x) != 0 || log(x + 1.0) != 0 || exp(x) != 0)
     {
-        if (strcmp(namefunc, "sin") == 0) printf("\nX = %f My func = %.8f Absolute error = %.8f Relative error = %.8f%%\n", x, func, fabsf(func - sin(x)), (fabsf(sin(x) - func) / fabsf(sin(x))) * 100.0);
-        if (strcmp(namefunc, "cos") == 0) printf("\nX = %f My func = %.8f Absolute error = %.8f Relative error = %.8f%%\n", x, func, fabsf(func - cos(x)), (fabsf(cos(x) - func) / fabsf(cos(x))) * 100.0);
-        if (strcmp(namefunc, "exp") == 0) printf("\nX = %f My func = %.8f Absolute error = %.8f Relative error = %.8f%%\n", x, func, fabsf(func - exp(x)), (fabsf(exp(x) - func) / fabsf(exp(x))) * 100.0);
-        if (strcmp(namefunc, "ln") == 0) printf("\nX = %f My func = %.8f Absolute error = %.8f Relative error = %.8f%%\n", x, func, fabsf(func - log(x + 1.0)), (fabsf(log(x + 1.0) - func) / fabsf(log(x + 1.0))) * 100.0);
+        printf("\nX = %f My func = %.8f Absolute error = %.8f Relative error = %.8f%%\n", x, func, fabsf(func - original), (fabsf(original - func) / fabsf(original)) * 100.0);
     }
     else
     {
-        if (strcmp(namefunc, "sin") == 0) printf("\nX = %f My func = %.8f Absolute error = %.8f Relative error = 0.00000000%\n", x, func, fabsf(func - sin(x)));
-        if (strcmp(namefunc, "cos") == 0) printf("\nX = %f My func = %.8f Absolute error = %.8f Relative error = 0.00000000%\n", x, func, fabsf(func - cos(x)));
-        if (strcmp(namefunc, "exp") == 0) printf("\nX = %f My func = %.8f Absolute error = %.8f Relative error = 0.00000000%\n", x, func, fabsf(func - exp(x)));
-        if (strcmp(namefunc, "ln") == 0) printf("\nX = %f My func = %.8f Absolute error = %.8f Relative error = 0.00000000%\n", x, func, fabsf(func - log(x + 1.0)));
+        printf("\nX = %f My func = %.8f Absolute error = %.8f Relative error = 0.00000000%\n", x, func, fabsf(func - original));
     }
 }
 
+enum summ_type
+{
+    direct,
+    reverse,
+    pairwise
+};
+
 int main()
 {
-    char namefunc[4];
+    float mas[100];
     float x = 0.0, func = 0.0;
-    int i, type;
-    printf("Enter the function name (sin, cos, exp, ln) : ");
-    scanf("%s", namefunc);
-    if (strcmp(namefunc, "sin") != 0 && strcmp(namefunc, "ln") != 0 && strcmp(namefunc, "cos") != 0 && strcmp(namefunc, "exp") != 0) { printf("ERROR"); return 0; }
+    int i, a;
+    summ_type type;
+    printf("Choose function:\n");
+    printf("Enter: 1 - sin(x), 2 - cos(x), 3 - exp(x), 4 - ln(1+x)\n");
+    scanf_s("%d", &a);
     printf("\nEnter x : ");
     scanf_s("%f", &x);
-    if ((strcmp(namefunc, "ln") == 0) && (x >= 1 || x <= -1)) { printf("ERROR: x must be in the range (-1;1)"); return 0; }
-    printf("Select the type of summation:\n1 - direct summation \n2 - reverse summation\n3 - pairwise summation\n");
-    scanf_s("\n%i", &type);
-    switch (type)
+    switch (a)
     {
     case(1):
-    {
-        func = directsum(namefunc, x);
-        printfunc(namefunc, func, x);
+        mas[0] = sin_ln_first(x);
+        create_mas(mas, sin_next, x);
+        printf("Select the type of summation:\n0 - direct summation \n1 - reverse summation\n2 - pairwise summation\n");
+        scanf_s("\n%i", &type);
+        switch (type)
+        {
+        case(direct):
+        {
+            func = directsum(mas);
+            printfunc(func, sin(x), x);
+            break;
+        }
+        case(reverse):
+        {
+            func = reversesum(mas);
+            printfunc(func, sin(x), x);
+            break;
+        }
+        case(pairwise):
+        {
+            func = pairwisesum(mas);
+            printfunc(func, sin(x), x);
+            break;
+        }
+        default:
+        {
+            printf("error");
+            break;
+        }
+        }
         break;
-    }
     case(2):
-    {
-        func = reversesum(namefunc, x);
-        printfunc(namefunc, func, x);
+        mas[0] = cos_exp_first();
+        create_mas(mas, cos_next, x);
+        printf("Select the type of summation:\n0 - direct summation \n1 - reverse summation\n2 - pairwise summation\n");
+        scanf_s("\n%i", &type);
+        switch (type)
+        {
+        case(direct):
+        {
+            func = directsum(mas);
+            printfunc(func, cos(x),  x);
+            break;
+        }
+        case(reverse):
+        {
+            func = reversesum(mas);
+            printfunc(func, cos(x), x);
+            break;
+        }
+        case(pairwise):
+        {
+            func = pairwisesum(mas);
+            printfunc(func, cos(x), x);
+            break;
+        }
+        default:
+        {
+            printf("error");
+            break;
+        }
+        }
         break;
-    }
     case(3):
-    {
-        func = pairwisesum(namefunc, x);
-        printfunc(namefunc, func, x);
+        mas[0] = cos_exp_first();
+        create_mas(mas, exp_next, x);
+        printf("Select the type of summation:\n0 - direct summation \n1 - reverse summation\n2 - pairwise summation\n");
+        scanf_s("\n%i", &type);
+        switch (type)
+        {
+        case(direct):
+        {
+            func = directsum(mas);
+            printfunc(func, exp(x), x);
+            break;
+        }
+        case(reverse):
+        {
+            func = reversesum(mas);
+            printfunc(func, exp(x), x);
+            break;
+        }
+        case(pairwise):
+        {
+            func = pairwisesum(mas);
+            printfunc(func, exp(x), x);
+            break;
+        }
+        default:
+        {
+            printf("error");
+            break;
+        }
+        }
         break;
-    }
+    case(4):
+        if (x >= 1 || x <= -1) { printf("ERROR: x must be in the range (-1;1)"); return 0; }
+        mas[0] = sin_ln_first(x);
+        create_mas(mas, ln_next, x);
+        printf("Select the type of summation:\n0 - direct summation \n1 - reverse summation\n2 - pairwise summation\n");
+        scanf_s("\n%i", &type);
+        switch (type)
+        {
+        case(direct):
+        {
+            func = directsum(mas);
+            printfunc(func, log(x + 1.0), x);
+            break;
+        }
+        case(reverse):
+        {
+            func = reversesum(mas);
+            printfunc(func, log(x + 1.0), x);
+            break;
+        }
+        case(pairwise):
+        {
+            func = pairwisesum(mas);
+            printfunc(func, log(x + 1.0), x);
+            break;
+        }
+        default:
+        {
+            printf("error");
+            break;
+        }
+        }
+        break;
     default:
     {
         printf("error");
