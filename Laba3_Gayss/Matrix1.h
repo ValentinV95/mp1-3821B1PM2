9d7d2d58850a612d2257454b926438741ee62a4c
JuliaAnisimovaa <94898957+JuliaAnisimovaa@users.noreply.github.com>
2022-06-02 17:46:44 +0300

    Update Matrix1.h

--- Laba3_Gayss/Matrix1.h
+++ Laba3_Gayss/Matrix1.h
@@ -2,70 +2,166 @@
 #include "ClassVector.h"
 #include <stdio.h>      
 #include <time.h>
+#include<iomanip>
 template <typename T>
 class Matrix : public Vector<Vector<T>> {
 public:
 	Matrix(size_t stolb, const Vector<T>& stroka) : Vector<Vector<T>>(stolb, stroka) {}
-};
 
-
-template <typename T>
-class Gauss : public Matrix<T> {
-public:
-	Gauss(size_t stolb, const Vector<T>& stroka) : Matrix<T>(stolb, stroka) {};
-
-
-Vector<T> Otvet(int &a) {
-
-	size_t razmer = this->GetSize(); 
-	Vector<T> otvet(razmer);
-	size_t max; 
-	a = 0;
-	int zero = 0;
-	
-	for (size_t i = 0; i < razmer; i++) {
-		zero = 0;
-		for (size_t j = i + 1; j < razmer; j++) 
-			if ((*this)[i][j] == 0) zero++;
-		if (zero == razmer) a=3;
-	}
-
-
-	for (size_t i = 0; i < razmer; i++) {
-		max = i;
-		for (size_t j = i + 1; j < razmer; j++) {
-			if ((*this)[i][j] * (*this)[i][j] > (*this)[i][max] * (*this)[i][max]) max = j;
-		}
-		swap<Vector<T>>((*this)[i], (*this)[max]); 
-		(*this)[i] = (*this)[i] / (*this)[i][i];
-		for (size_t j = i + 1; j < razmer; j++) (*this)[j] = (*this)[j] - ((*this)[i] * (*this)[j][i]);
-	}
-
-	for (int i = razmer - 1; i >= 0; i--) {
-		otvet[i] = (*this)[i][razmer];
-		for (int j = razmer - 1; j >= i + 1; j--)   otvet[i] = otvet[i] - (*this)[i][j] * otvet[j];
-	}
-	int check = 1;
-	for (int i = 0; i < razmer; i++)
-		check = check * (*this)[i][i];
-	if (check != 0)
-		return otvet;
-	else {
-		a=1;
-		return otvet;
-	}
-}
-void random()
-{
-	size_t raz = this->GetSize();
-	srand(time(NULL));
-	for (int i = 0; i < raz; i++)
-		for (int j = 0; j < raz+1; j++)
+	const Vector<T> operator * (Vector<T>& other)
+	{
+		Vector<T> res(this->size);
+		for (int i = 0; i < this->size; i++)
 		{
-			(*this)[i][j] = rand();
+			res[i] = this->mas[i] * other;
 		}
-	
-}
+		return res;
+	}
 };
+
+class Gauss {
+private:
+	size_t size;
+	Matrix<double> mat;
+	Vector<double> vec;
+	Vector<double> res;
+public:
+	Gauss(size_t stolb, const Vector<double>& stroka) : mat(stolb, stroka), vec(stolb), res(stolb)
+	{
+		this->size = stolb;
+	}
+
+	size_t GetSize() { return size; }
+
+	Vector<double>& Solve()
+	{
+		Matrix<double> matCopy(mat);
+		Vector<double> vecCopy(vec);
+
+		for (int j = 0; j < size; j++)
+		{
+			double k;
+			int maxJ = j;
+			for (int i = j + 1; i < size; i++)
+			{
+				if (abs(mat[maxJ][j]) < abs(mat[i][j]))
+				{
+					maxJ = i;
+				}
+			}
+			if (j != maxJ)
+			{
+				swap(mat[j], mat[maxJ]);
+				swap(mat[j], mat[maxJ]);
+			}
+			if (mat[maxJ][j] == 0)
+			{
+				throw exception("Система несовместна");
+			}
+			for (int i = j + 1; i < size; i++)
+			{
+				if (mat[j][j] != 0)
+				{
+					k = mat[i][j] / mat[j][j];
+				}
+				else
+				{
+					throw exception("деление на 0");
+				}
+				mat[i] -= mat[j] * k;
+				vec[i] -= vec[j] * k;
+			}
+		}
+		for (int i = size - 1; i >= 0; i--)
+		{
+			double summ = 0;
+			for (int j = i + 1; j < size; j++)
+			{
+				summ += mat[i][j] * res[j];
+			}
+			res[i] = (vec[i] - summ) / mat[i][i];
+		}
+
+		if (examination(matCopy, vecCopy))
+		{
+			cout << "Решено!\n" << endl;
+		}
+		else
+		{
+			cout << "Решено с ошибкой!\n" << endl;
+		}
+		return res;
+	}
+	bool examination(Matrix<double>& Mat, Vector<double>& Vec)
+	{
+		Vector<double> exam = Mat * res;
+		for (int i = 0; i < size; i++)
+		{
+			if (abs(exam[i] - Vec[i]) >= 0.0000000001)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	void random()
+	{
+		double low = -5.0;
+		double max = 5.0;
+
+		srand(static_cast <unsigned> (time(0)));
+
+		for (int i = 0; i < size; i++)
+		{
+			vec[i] = low + ((double)(rand()) / ((double)(RAND_MAX / (max - low))));
+			for (int j = 0; j < size; j++)
+			{
+				mat[i][j] = low + ((double)(rand()) / ((double)(RAND_MAX / (max - low))));
+				if (i == j)
+					mat[i][j] = size * max + ((double)(rand()) / ((double)(RAND_MAX / (size * max))));
+			}
+		}
+	}
+	friend ostream& operator << (ostream& out, Gauss& system);
+	friend istream& operator >> (istream& in, Gauss& system);
+};
+ostream& operator << (ostream& out, Gauss& system)
+{
+	cout << std::fixed;
+	for (int i = 0; i < system.GetSize(); i++)
+	{
+		cout << "|";
+		for (int j = 0; j < system.GetSize(); j++)
+		{
+			cout.width(10);
+			cout << std::setprecision(5)
+				<< system.mat[i][j] << ' ';
+		}
+		cout << " |";
+		cout.width(10);
+		cout << std::setprecision(5)
+			<< system.vec[i] << "  |" << '\n';
+	}
+	cout << "\n\n";
+	return out;
+}
+istream& operator >> (istream& in, Gauss& system)
+{
+	for (int i = 0; i < system.GetSize(); i++)
+	{
+		for (int j = 0; j < system.GetSize(); j++)
+		{
+			cout << "mat[" << i + 1 << "][" << j + 1 << "] = ";
+			cin >> system.mat[i][j];
+		}
+	}
+	for (int i = 0; i < system.size; i++)
+	{
+		cout << "vec[" << i + 1 << "] = ";
+		cin >> system.vec[i];
+	}
+	return in;
+}
+
 
 
